c3073e1394ccffd865dfffc4cb3a5db29969b455
dmytrochyharev <d.chyharev@lerpal.com>
2023-10-26 23:53:43 +0300

    user view fix

--- user/rest/serializers.py
+++ user/rest/serializers.py
@@ -3,12 +3,7 @@ from rest_framework import serializers
 from user.models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password', 'last_activity', 'last_login']
-        extra_kwargs = {'password': {'write_only': True}}
-
+class UserRequestSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User(
             username=validated_data['username'],
@@ -17,3 +12,14 @@ class UserSerializer(serializers.ModelSerializer):
         user.set_password(validated_data['password'])
         user.save()
         return user
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+
+class UserResponseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'last_activity', 'last_login']
--- user/rest/views.py
+++ user/rest/views.py
@@ -1,11 +1,20 @@
+from rest_framework import status
+from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
 
+from post.rest.views import GenericViewSet
 from user.models import User
-from user.rest.serializers import UserSerializer
+from user.rest.serializers import UserRequestSerializer, UserResponseSerializer
 
 
-class UserCreate(ModelViewSet):
+class UserCreate(GenericViewSet):
+    request_serializer_class = UserRequestSerializer
+    response_serializer_class = UserResponseSerializer
     queryset = User.objects.all()
-    serializer_class = UserSerializer
     permission_classes = (AllowAny, )
+
+    @action(methods=['get'], url_path='detail', detail=True)
+    def user_detail(self, request, *args, **kwargs):
+        serializer = self.get_response_serializer(self.get_object())
+        return Response(serializer.data, status=status.HTTP_200_OK)
