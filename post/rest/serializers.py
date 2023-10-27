dc9b5cd851465ff021889d379886ee70e2ee4b56
dmytrochyharev <d.chyharev@lerpal.com>
2023-10-27 12:15:58 +0300

    post view fix

--- post/rest/serializers.py
+++ post/rest/serializers.py
@@ -4,7 +4,6 @@ from post.models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
-    self = serializers.HyperlinkedIdentityField(view_name='post-detail')
     likes_count = serializers.SerializerMethodField()
 
     def get_likes_count(self, post_object):
@@ -19,4 +18,4 @@ class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = ('id', 'self', 'author', 'title', 'content', 'date', 'likes_count')
+        fields = ('id', 'author', 'title', 'content', 'date', 'likes_count')
